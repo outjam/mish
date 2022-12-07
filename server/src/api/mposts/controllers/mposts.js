ac5683f58ee33b1c4307a83d115435d8127368cf
outjam <38182485+outjam@users.noreply.github.com>
2022-12-07 13:13:12 +0300

    Upd

---
+++ server/src/api/mposts/controllers/mposts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mposts controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mposts.mposts');
---
+++ server/src/api/mposts/routes/mposts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mposts router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mposts.mposts');
